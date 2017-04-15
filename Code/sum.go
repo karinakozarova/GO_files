958ed008deb1610d4d93aa6096bd6e5741e372a8
Karina Kozarova <karinakozarova1303@gmail.com>
2017-04-15 19:09:48 +0300

    Create sum.go

---
+++ Code/sum.go
@@ -0,0 +1,13 @@
+package main
+import "fmt"
+func main() {
+		var a int
+		var b int
+		fmt.Printf("Enter num1 ")
+		fmt.Scanf("%v",&a)
+		fmt.Printf("Enter num2 ")
+		fmt.Scanf("%v",&b)
+		fmt.Printf("Sum: %v" , a+b)
+		
+}
+

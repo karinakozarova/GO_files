cdd5b1a2c26c52139abec7ddd260dd44a1aa0d4e
Karina Kozarova <karinakozarova1303@gmail.com>
2017-04-18 21:25:46 +0300

    Create printing_scanning.go

---
+++ Code/printing_scanning.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+
+func main () {
+		fmt.Print("Print\n")
+		fmt.Println("Println")
+		fmt.Printf("Printf\n")
+		 i := 0
+		fmt.Scan("%v",&i)
+		fmt.Println("%v",i)
+		fmt.Scanf("%v",i)
+		fmt.Println("%v",i)
+		fmt.Scanln("%v",i)
+		fmt.Println("%v",i)
+
+
+}
